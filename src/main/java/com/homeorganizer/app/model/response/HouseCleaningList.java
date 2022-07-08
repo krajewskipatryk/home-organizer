a0de1103bfad6ed2b8b4b3083ace47c4b6785b60
patryk <krajewskipatryk@onet.pl>
2022-07-08 22:09:39 +0200

    cleaning redesign start

--- src/main/java/com/homeorganizer/app/model/response/HouseCleaningList.java
+++
@@ -1,9 +0,0 @@
-package com.homeorganizer.app.model.response;
-
-import com.homeorganizer.app.dto.HouseCleanerDto;
-
-import java.util.ArrayList;
-
-public class HouseCleaningList {
-    private ArrayList<HouseCleanerDto> houseCleaningList;
-}
